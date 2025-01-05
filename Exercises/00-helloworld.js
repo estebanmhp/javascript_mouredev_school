efed9723918b4a8449cc6b25ee95f72b09b6d39f
estebanmhp <estebanhernandezpe@gmail.com>
2025-01-04 19:07:25 -0500

    Adding git ignore file and the first exercise in JS, hello world

---
+++ Exercises/00-helloworld.js
@@ -0,0 +1,29 @@
+// This is a simple comment in a single line
+
+/*
+This is 
+a multi-line
+comment
+*/
+
+console.log("Hello, JavaScript!")
+console.log('Helloooo, JavaScript')
+console.log(`Hello, JavaScript`)
+
+// console.log("Hello, world!!!")
+
+/*
+console.log("Hello, world!")
+console.log('Hello, world!')
+console.log(`Hello, world!`)
+*/
+
+console.log(5)
+console.log("5")
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 ** 2)
+console.log(5 / 2)
+console.log(5 % 2)
+
