bea302afef879bbab9b50edd3f125bcdc55c9a39
estebanmhp <estebanhernandezpe@gmail.com>
2025-01-04 21:33:52 -0500

    Primitive data types

---
+++ Exercises/02-datatypes.js
@@ -0,0 +1,39 @@
+//PRIMITIVE DATA TYPES
+
+//string
+let user = "Esteban Example"
+let email = "estebanexample@email.com"
+
+//number
+let age = 22 // Integer
+let height = 1.76 // Floating number
+
+//boolean
+let man = true
+let woman = false
+
+//undefined
+let undefinedValue
+
+//null
+let nullValue = null
+
+//symbol
+let symbolValue = Symbol("mySymbol")
+
+//bigInt
+let bigIntValue = 1407n
+let bigInt2 = BigInt(487165198419518918919192972659762947249527957979795561312136444)
+
+//show the data type (typeof)
+console.log(typeof user)
+console.log(typeof email)
+console.log(typeof age)
+console.log(typeof height)
+console.log(typeof man)
+console.log(typeof woman)
+console.log(typeof undefinedValue)
+console.log(typeof nullValue)
+console.log(typeof symbolValue)
+console.log(typeof bigIntValue)
+console.log(typeof bigInt2)
